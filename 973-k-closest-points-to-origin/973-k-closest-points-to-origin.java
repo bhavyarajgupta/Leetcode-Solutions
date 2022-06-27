ea2144b2079814d3c85b92476ed99e787f01b4dd
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-06-27 11:43:10 +0530

    Time: 74 ms (23.25%), Space: 117.2 MB (53.07%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,37 +1,42 @@
 class Solution {
     
-    static class Pair implements Comparable<Pair>{
-        int x, y;
-        double distancefromorigin;
-        
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-            distancefromorigin = Math.sqrt(x*x + y*y);
-        }
-        
-        @Override
-        public int compareTo(Pair pair) {
-            if(distancefromorigin < pair.distancefromorigin)
+     
+    static class Pair{
+       public int x,y;
+    }
+    
+    static class Euclidian implements Comparator<Pair>{
+        public int compare(Pair c1, Pair c2){
+            double d1 = Math.sqrt(c1.x*c1.x + c1.y*c1.y);
+            double d2 = Math.sqrt(c2.x*c2.x + c2.y*c2.y);
+            
+            if(d1<d2){
                 return -1;
-            return 1;
+            }else if(d1>d2){
+                return 1;
+            }
+            
+            return 0;
         }
     }
     
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        ArrayList<Pair> list = new ArrayList<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new Euclidian());
         
-        for(int[] point : points) {
-            list.add(new Pair(point[0], point[1]));
-        }
         
-        pq.addAll(list);
+    for(int i=0;i<points.length;i++){
+        Pair p = new Pair();
+        p.x = points[i][0];
+        p.y=  points[i][1];
+        pq.add(p);
+    }
+        
+        
         
         int[][] ans = new int[k][2];
         
         int idx = 0;
-        while(k --> 0) {
+        while(idx<k) {
             Pair temp = pq.remove();
             ans[idx][0] = temp.x;
             ans[idx][1] = temp.y;
