ca61e7d201830a991d3ca2186a4f47b3a46e901c
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-06-28 10:27:11 +0530

    Time: 499 ms (14.93%), Space: 166.8 MB (5.03%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -13,44 +13,21 @@ class Solution {
         
         for(int i=0;i<s.length()-wordlen+1;i++){
             String check = s.substring(i,i+wordlen);
-            // int store = i+wordlen;
-            // System.out.println(i + " " + store +" "+check);
-            // int count = 0;
+           
             
             HashMap<String,Integer> temp = new HashMap<>();
-            // System.out.println(check.length());
+      
             for(int j=0;j<check.length()-len+1;j=j+len){
                 if(map.containsKey(check.substring(j,j+len))){  
-                                    // System.out.println(check.substring(j,j+len));
+                                    
                     temp.put(check.substring(j,j+len),temp.getOrDefault(check.substring(j,j+len),0)+1);  
         
                                                              
                 }
               
             }
-            // temp.putAll(map);
-            // System.out.println(i + "  " + temp.size() +"  "+ map.size());
             
-           
-         
-            // for(String st: map.keySet()){
-//             for(String st:words){
-//                 System.out.println(i);
-//                 if(map.get(st) > 0 && check.contains(st) ) {
-//                     System.out.println(map + st);
-//                     map.put(st,map.get(st)-1);
-//                     count++;                    
-                    
-//                 }
-                
-//             }
-            
-            // map.putAll(temp);
-            
-            // if(count == words.length){
             if(temp.equals(map)){
-                 
-                // System.out.println("lastcount "+ count);
                 res.add(i);
             }
             
