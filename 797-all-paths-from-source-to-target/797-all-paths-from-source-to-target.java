dde7f5aa98d970f77bf320f60347f4c6ac4de723
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-07-02 16:19:25 +0530

    Time: 5 ms (52.47%), Space: 54.3 MB (80.59%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -0,0 +1,32 @@
+class Solution {
+    // List<List<Integer>> res;
+    
+    public void DFS(int src, int desc,boolean [] vis,int[][] graph,List<Integer>              path,List<List<Integer>> paths){
+        
+        if(vis[src] == true) return;
+        
+        vis[src] = true;
+        path.add(src);
+        
+        if(src == desc){
+            paths.add(new ArrayList<>(path));       //DEEP COPY
+        }
+        
+        for(int nbr:graph[src]){
+            DFS(nbr,desc,vis,graph,path,paths);
+        }
+        
+        vis[src] = false;
+        path.remove(path.size()-1);
+    }
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<Integer> path = new ArrayList<>();
+        List<List<Integer>> paths = new ArrayList<>();
+        
+        boolean [] vis = new boolean[graph.length];
+        
+        DFS(0,graph.length-1,vis,graph,path,paths);
+        
+        return paths;
+    }
+}
