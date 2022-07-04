fa71959811b51a8c9acc1abbe626945710fe11c6
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-07-04 23:54:26 +0530

    Time: 5 ms (45.91%), Space: 51.8 MB (50.80%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        
+        int ans = 0;
+        int[] candies = new int[ratings.length];
+        
+        Arrays.fill(candies, 1);
+        
+        for(int i = 1; i < ratings.length; i++) {
+            
+            if(ratings[i] > ratings[i-1])
+                candies[i] = candies[i-1]+1;
+            
+        }
+        
+        for(int i = ratings.length - 2; i >= 0; i--) {
+            
+            if(ratings[i] > ratings[i+1])
+                candies[i] = Math.max(candies[i], candies[i+1]+1);
+            
+        }
+        
+        for(int candy : candies)
+            ans += candy;
+        
+        return ans;
+    }
+}
