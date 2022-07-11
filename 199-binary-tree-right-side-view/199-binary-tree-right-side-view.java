2fa5cc0b15c6e2cf64c3bf58c6e232c6af39b189
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-07-11 12:41:03 +0530

    Time: 1 ms (95.06%), Space: 43.1 MB (23.10%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -16,53 +16,61 @@
 
 class Solution {
     
-//     public void rightside(TreeNode root,List<Integer> ans){
-//         if(root == null) return;
-        
-//         if(root.right != null)
-//             ans.add(root.right.val);
-        
-//         rightside(root.right,ans);
-//     }
-    
-//      public void leftside(TreeNode root,List<Integer> ans){
-//         if(root == null) return;
-        
-//         if(root.left != null)
-//             ans.add(root.left.val);
-        
-//         leftside(root.left,ans);
-//     }
-    
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans= new ArrayList<>();
-        
-        
-        if(root == null) return ans;
-        
-        ArrayDeque<TreeNode > q = new ArrayDeque<>();
-        
-        q.add(root);
-        
-        while(q.size() > 0){
-            
-            ans.add(q.peekLast().val); 
-            
-            int size = q.size();
-            
-            while(size-->0){
-                TreeNode curr = q.remove();
-                
-                if(curr.left != null){
-                    q.add(curr.left);
-                }
-                
-                if(curr.right != null){
-                    q.add(curr.right);
-                }
+    public  List<Integer> BFS(TreeNode root){
+     List<Integer> ans= new ArrayList<>();
+
+
+    if(root == null) return ans;
+
+    ArrayDeque<TreeNode > q = new ArrayDeque<>();
+
+    q.add(root);
+
+    while(q.size() > 0){
+
+        ans.add(q.peekLast().val); 
+
+        int size = q.size();
+
+        while(size-->0){
+            TreeNode curr = q.remove();
+
+            if(curr.left != null){
+                q.add(curr.left);
+            }
+
+            if(curr.right != null){
+                q.add(curr.right);
             }
         }
+    }
+
+        return ans;
+    }
+    
+    
+    
+    
+    public void DFS(List<Integer> ans , TreeNode root,int depth){
+        
+        if(root == null) return;
+        
+        if(ans.size() == depth) ans.add(root.val);
+        
+        DFS(ans,root.right,depth+1);
+        DFS(ans,root.left,depth+1);
+
+        
+        
+    }
+    
+
+    public List<Integer> rightSideView(TreeNode root) {
+       // return BFS(root);
+        List <Integer> ans = new ArrayList<>();
+        DFS(ans,root,0);
         
         return ans;
+        
     }
 }
