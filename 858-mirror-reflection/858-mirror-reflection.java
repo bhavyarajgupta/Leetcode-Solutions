5d40c4708e9c60e903caee36b919f19cc2526b75
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-08-05 00:15:07 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (80.56%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -1,9 +1,22 @@
 class Solution {
      public int mirrorReflection(int p, int q) {
-        while(((p|q)&1) == 0){
-            p >>= 1;
-            q >>= 1;
+        // while(((p|q)&1) == 0){
+        //     p >>= 1;
+        //     q >>= 1;
+        // }
+        // return (q&1) + ((p&1)^1);
+         
+        while ((p%2==0) && (q%2==0)) {
+            p/=2;
+            q/=2;
         }
-        return (q&1) + ((p&1)^1);
+        //both p qnd q can't be even
+        if((p%2)==0 && (q%2)!=0){
+            return 2;//when p is even and q is odd
+        } 
+        if((p%2)!=0 && (q%2)!=0){
+            return 1;// when p is odd and q is odd
+        } 
+        return 0;// when p is odd and q is even
     }
 }
