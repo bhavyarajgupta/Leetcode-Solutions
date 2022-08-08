ba0fdffa8a887a0c71a50eb7aa2e13c13cbc37ac
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-08-08 20:01:05 +0530

    Time: 15 ms (40.32%), Space: 99 MB (41.69%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,28 +9,65 @@
  * }
  */
 class Solution {
+    
+    public ListNode mid(ListNode head){
+        ListNode fast = head.next;
+        ListNode slow = head;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        return slow;
+    }
+   
+    public ListNode reverse(ListNode head){
+        if(head == null || head.next == null) return head;
+        
+        ListNode temp = reverse(head.next);
+        
+        head.next.next = head;
+        head.next = null;
+        
+        return temp;
+    }
 
     public boolean isPalindrome(ListNode head) {
         
-        ArrayList<Integer> list = new ArrayList<>();
+//         ArrayList<Integer> list = new ArrayList<>();
         
-        while(head != null){
-            list.add(head.val);
+//         while(head != null){
+//             list.add(head.val);
+//             head = head.next;
+//         }
+        
+        
+//         int i =0;
+//         int j= list.size()-1;
+        
+//         while(i<j){
+//             if(list.get(i) != list.get(j)){
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+        
+        // return true;
+        
+        ListNode mid = mid(head);
+        
+        ListNode reverse = reverse(mid.next);
+        mid.next = null;
+               
+        while(head != null && reverse != null){
+            if(head.val != reverse.val) return false;
+            
             head = head.next;
+            reverse = reverse.next;
         }
+        return true;       
         
-        
-        int i =0;
-        int j= list.size()-1;
-        
-        while(i<j){
-            if(list.get(i) != list.get(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        
-        return true;
     }
 }
