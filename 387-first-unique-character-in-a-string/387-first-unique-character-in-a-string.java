f9d4338c79498ccceafe66477dfa805003fe0f20
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-08-16 11:44:20 +0530

    Time: 10 ms (84.43%), Space: 50 MB (50.26%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,19 +1,36 @@
 class Solution {
     public int firstUniqChar(String s) {
         
-        HashMap<Character,Integer> map = new LinkedHashMap<>();
+//         HashMap<Character,Integer> map = new LinkedHashMap<>();
         
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+//         for(int i=0;i<s.length();i++){
+//             char ch = s.charAt(i);
+//             map.put(ch,map.getOrDefault(ch,0)+1);
+//         }
+        
+//         for(int i=0;i<s.length();i++){
+//             char ch = s.charAt(i);
+            
+//             if(map.get(ch) == 1){
+//                 return i;
+//             }
+//         }
+        
+//         return -1;
+        
+        int[] count = new int[26];
+        
+        
+        for (char c: s.toCharArray()) {
+            count[c - 'a']++;
         }
         
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
+        for (int i = 0; i < s.length(); i++) {
             
-            if(map.get(ch) == 1){
+            if (count[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
+            
         }
         
         return -1;
