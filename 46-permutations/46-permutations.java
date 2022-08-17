fc06a2313f11c2d1d75e527d966105ce2f4043c1
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-08-17 18:21:35 +0530

    Time: 2 ms (77.04%), Space: 44.5 MB (71.64%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,27 +1,52 @@
 class Solution {
     
-    void recPerm(int[] nums,List<Integer> ds,List<List<Integer>> ans,boolean[] freq){
-        if(ds.size() == nums.length){
+//     void recPerm(int[] nums,List<Integer> ds,List<List<Integer>> ans,boolean[] freq){
+//         if(ds.size() == nums.length){
+//             ans.add(new ArrayList<>(ds));
+//             return;
+//         }   
+        
+//         for(int i=0;i<nums.length;i++){
+//             if(!freq[i]){
+//                 freq[i] = true;
+//                 ds.add(nums[i]);
+//                 recPerm(nums,ds,ans,freq);
+//                 ds.remove(ds.size()-1);
+//                 freq[i] = false;
+//             }
+//         }
+        
+//     }
+    
+    void recPerm(int index,int[] nums,List<List<Integer>> ans){
+        if(index == nums.length){
+            //copy swapped array element to ds and then to ans
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                ds.add(nums[i]);
+            }
             ans.add(new ArrayList<>(ds));
             return;
-        }   
-        
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i] = true;
-                ds.add(nums[i]);
-                recPerm(nums,ds,ans,freq);
-                ds.remove(ds.size()-1);
-                freq[i] = false;
-            }
         }
         
+        for(int i=index;i<nums.length;i++){
+            swap(i,index,nums);
+            recPerm(index+1,nums,ans);
+            swap(i,index,nums);
+        }
+    }
+    
+    void swap(int i,int j,int nums[]){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         boolean freq[] = new boolean[nums.length];
-        recPerm(nums,new ArrayList<>(),ans,freq);
+        recPerm(0,nums,ans);
+        // recPerm(nums,new ArrayList<>(),ans,freq);
         return ans;    
     }
     
