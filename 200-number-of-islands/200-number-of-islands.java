ad763faebb25f7c168a028aedd9f7dc07edb7ee2
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-08-29 09:15:46 +0530

    Time: 3 ms (97.77%), Space: 58.1 MB (18.46%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,34 +1,30 @@
 class Solution {
-    public void DFS(int r, int c, char[][] grid){
-        //out of matrix , water cell , visited land
-        if(r<0||c<0||r>=grid.length||c>=grid[0].length||grid[r][c] =='0' || grid[r][c] == 'N') return ;
+    
+    void dfs(int i,int j,char[][] grid){
+        if(i<0 || j<0 || i > grid.length-1 || j>grid[0].length-1 || grid[i][j] == '0' || grid[i][j] == 'X'){
+            return ;
+        }
         
-        grid[r][c] ='N';  //visited land
+        grid[i][j] = 'X';
         
-        
-        //DFS on each side
-        DFS(r+1,c,grid);
-        DFS(r-1,c,grid);
-        DFS(r,c+1,grid);
-        DFS(r,c-1,grid);
-        
+        dfs(i+1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
     }
     
-    
     public int numIslands(char[][] grid) {
-        int island = 0 ; //no of island;
         
-        
-        //Loop matrix
+        int ans = 0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j] == '1'){
-                    DFS(i,j,grid);
-                    island++;
+                    dfs(i,j,grid);
+                    ans++;
                 }
             }
         }
         
-        return island;
+        return ans;
     }
 }
