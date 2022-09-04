48765024dc3f32845625ea2d66bee118f8712718
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-09-04 13:51:39 +0530

    Time: 3 ms (80.44%), Space: 42.5 MB (76.97%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,32 +1,32 @@
 class Solution {
-
-    public int[] numsSameConsecDiff(int N, int K) {
-
-        if (N == 1)
-            return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        List<Integer> queue = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        for(int level = 1; level < N; ++ level) {
-            ArrayList<Integer> nextQueue = new ArrayList<>();
-            // iterate through each number within the level
-            for (Integer num : queue) {
-                Integer tailDigit = num % 10;
-
-                ArrayList<Integer> nextDigits = new ArrayList<>();
-                nextDigits.add(tailDigit + K);
-                if (K != 0)
-                    nextDigits.add(tailDigit - K);
-                for (Integer nextDigit : nextDigits) {
-                    if (0 <= nextDigit && nextDigit < 10) {
-                        Integer newNum = num * 10 + nextDigit;
-                        nextQueue.add(newNum);
-                    }
-                }
-            }
-            // prepare for the next level
-            queue = nextQueue;
+    
+    ArrayList<Integer> ans = new ArrayList<>();
+    
+    private void dfs(int n, int k, int num, int digits) {
+        
+        if(digits == n) {
+            ans.add(num);
+            return;
         }
-
-        return queue.stream().mapToInt(i->i).toArray();
+        
+        int count = 0;
+        while(count <= 9) {
+            if(Math.abs(num%10 - count) == k) {
+                dfs(n, k, num*10 + count, digits+1);
+            }
+            count++;
+        }
+        
+    }
+    
+    public int[] numsSameConsecDiff(int n, int k) {
+        for(int i = 1; i <= 9; i++)
+            dfs(n, k, i, 1);
+        
+        int[] nums = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++)
+            nums[i] = ans.get(i);
+        
+        return nums;
     }
 }
