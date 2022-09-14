0a173136c1bbc5f1dd80afabac252e383e77f13b
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-09-14 12:38:14 +0530

    Time: 88 ms (25.15%), Space: 133.9 MB (16.96%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    
+    public void r2npath(TreeNode root , HashMap<Integer,Integer> map){
+        if(root == null) return;
+        
+        map.put(root.val,map.getOrDefault(root.val,0)+1);
+        
+        if(root.left == null && root.right == null){
+            boolean flag = true;
+            int odd = 0;
+            for(int val:map.keySet()){
+                if((map.get(val) & 1) == 1){
+                    odd++;
+                    if(odd > 1 ){
+                        flag = false;
+                        break;
+                    }
+                    
+                }
+            }
+            
+            if(flag == true){
+                count++;
+            }
+            
+            map.put(root.val,map.get(root.val)-1);
+            
+            return;
+        }
+        
+        r2npath(root.left,map);
+        r2npath(root.right,map);
+        map.put(root.val,map.get(root.val)-1);
+    }
+    
+    public int pseudoPalindromicPaths (TreeNode root) {
+        
+        r2npath(root,new HashMap<>());
+        
+        return count;
+    }
+}
