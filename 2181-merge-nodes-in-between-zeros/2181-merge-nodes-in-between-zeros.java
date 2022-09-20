a14a56915f7cb829314afbd686d2e90bc07aae90
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-09-20 18:42:48 +0530

    Time: 15 ms (31.46%), Space: 299.2 MB (48.86%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -13,24 +13,45 @@ class Solution {
         ListNode dummy = new ListNode();
         ListNode tail = dummy;
         
-        while(head != null && head.next != null){
-            if(head.val == 0){
-                // ListNode temp = head.next;
-                ListNode prev = head.next;
-                int sum = 0;
-                while(prev != null && prev.val != 0){
-                    sum += prev.val;
-                    prev = prev.next;
-                }
+//         while(head != null && head.next != null){
+//             if(head.val == 0){
+//                 ListNode prev = head.next;
+//                 int sum = 0;
+//                 while(prev != null && prev.val != 0){
+//                     sum += prev.val;
+//                     prev = prev.next;
+//                 }
                 
+//                 tail.next = new ListNode(sum);
+//                 tail = tail.next;
+//                 head = prev;
+//             }
+//             else{
+//                     head = head.next;
+//             }
+//         }
+        
+        int toggle = 0;
+        int sum = 0;
+        while(head != null){
+            if(head.val == 0){
+                toggle++;
+            }
+            
+            if(toggle == 1){
+                sum+=head.val;
+                // Systee.out.println(sum);
+            }
+            
+            if(toggle == 2){
                 tail.next = new ListNode(sum);
                 tail = tail.next;
-                head = prev;
+                sum = 0;
+                toggle--;
             }
-            else{
-                // if(head.next != null) 
-                    head = head.next;
-            }
+            
+            
+            head = head.next;
         }
         
         tail.next = null;
