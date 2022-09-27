e15fe48636696c9eb93a5e53353e63ea847ada53
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-09-28 00:05:14 +0530

    Time: 6 ms (21.66%), Space: 46.4 MB (16.63%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -11,13 +11,27 @@ class Solution {
         int left = grid[i][j] + recMemo(i,j-1,grid,dp);
         
         return dp[i][j] = Math.min(left,up);
-    }    
+    }  
+    
+    
     public int minPathSum(int[][] grid) {
         int dp[][] = new int[grid.length][grid[0].length];
-        for(int i[]:dp){
-            Arrays.fill(i,-1);
+        
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(i == 0 && j == 0) dp[i][j] = grid[0][0];
+                else{
+                    int left = Integer.MAX_VALUE;
+                    int up = Integer.MAX_VALUE;
+                    
+                    if(i>0) up = grid[i][j] + dp[i-1][j];
+                    if(j>0) left = grid[i][j] + dp[i][j-1];
+                    
+                    dp[i][j] = Math.min(left,up);
+                }  
+            }
         }
         
-        return recMemo(grid.length-1,grid[0].length-1,grid,dp);
+        return dp[grid.length-1][grid[0].length-1];
     }
 }
