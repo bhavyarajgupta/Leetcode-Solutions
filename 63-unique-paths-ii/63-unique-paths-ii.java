c7ecb830c4d8fc24dffbd9b8a55989f9f4a68197
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-09-27 22:53:31 +0530

    Time: 2 ms (20.95%), Space: 42.5 MB (33.14%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -20,12 +20,21 @@ class Solution {
         
         int [][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
         
-        for(int i[]:dp){
-            Arrays.fill(i,-1);
+        for(int i=0;i<obstacleGrid.length;i++){
+            for(int j=0;j<obstacleGrid[0].length;j++){
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int up =0,left =0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    
+                    dp[i][j] = up+left;
+                }
+            }
         }
         
-        return memorec(obstacleGrid.length-1,obstacleGrid[0].length-1,obstacleGrid,dp);
-        
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];       
         
     }
 }
