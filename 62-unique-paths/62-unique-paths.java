3dfceb746bd6156908687cdb43726f810173505d
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-09-27 14:20:50 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (21.71%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,40 +1,16 @@
 class Solution {
-    
-    public int recMemo(int i,int j,int dp [][]){
-        if( i == 0 && j == 0) return 1;
-        if( i < 0 || j < 0) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int up = recMemo(i-1,j,dp);
-        int left = recMemo(i,j-1,dp);
-        
-        return dp[i][j] = up+left;
-    }
-    
+  
     public int uniquePaths(int m, int n) {
         
-        int prev[] = new int [n];
-        Arrays.fill(prev,0);
-        for(int i = 0;i<m;i++){
-            int cur[] = new int [n];
-            Arrays.fill(cur,0);
-            for(int j = 0;j<n;j++){
-                if(i == 0 && j == 0) cur[j] = 1;
-                else{
-                    int up = 0;
-                    int left = 0;
-                        if(i>0) up = prev[j];
-                        if(j>0) left = cur[j-1];
-                    cur[j]= up+left;
-                }
-            }
-            
-            prev = cur;
+        int N = n+m-2;
+        int r = m-1;
+        double res =1;
+        
+        for(int i=1;i<=r;i++){
+            res = res *(N-r+i)/i;
         }
         
-        
-        return prev[n-1];
+        return (int)res;
         
     }
 }
