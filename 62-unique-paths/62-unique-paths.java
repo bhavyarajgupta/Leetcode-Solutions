7a7c585baf941259da01bcb0dd90dae861e92785
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-09-27 14:05:47 +0530

    Time: 1 ms (56.38%), Space: 40.9 MB (58.45%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -16,10 +16,23 @@ class Solution {
         
         int dp[][] = new int[m][n];
         
-        for(int []x:dp){
-            Arrays.fill(x,-1);
+        // dp[0][0] = 1;
+        
+        for(int i =0 ;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int up = 0;
+                    int left = 0;
+                        if(i>0) up = dp[i-1][j];
+                        if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                    }
+            }
         }
         
-        return recMemo(m-1,n-1,dp);
+        
+        return dp[m-1][n-1];
+        
     }
 }
