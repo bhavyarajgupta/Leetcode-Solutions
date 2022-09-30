1d96cea4f222012b910902904d2593571f22c2e1
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-09-30 23:06:57 +0530

    Time: 88 ms (17.76%), Space: 49 MB (31.63%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,46 +1,52 @@
 class Solution {
-    
-    private int memo(int i,int j1,int j2,int[][] grid,int[][][] dp){
-        
-        if(j1 < 0 || j2 <0 || j1 >= grid[0].length || j2>=grid[0].length)
-            return (int)(Math.pow(-10,9));
-            
-        if(i == grid.length-1){
-            if(j1 == j2) return grid[i][j1];
-            else return grid[i][j1]+grid[i][j2];
-        }
-        
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2]; 
-        
-        int maxi = Integer.MIN_VALUE;
-        
-        for(int dj1 = -1 ; dj1 <= 1;dj1++){
-            for(int dj2 = -1 ; dj2 <= 1;dj2++){
-                
-                int ans = 0;
-                if(j1 == j2 ) 
-                    ans = grid[i][j1]+memo(i+1,j1+dj1,j2+dj2,grid,dp); 
-                else 
-                    ans = grid[i][j1]+grid[i][j2]+memo(i+1,j1+dj1,j2+dj2,grid,dp); 
-                
-                maxi = Math.max(maxi,ans);
-            }
-        }
-        
-        return dp[i][j1][j2] = maxi;
-        
-    }
-    
+       
     public int cherryPickup(int[][] grid) {
         int dp[][][] = new int[grid.length][grid[0].length][grid[0].length];
+        int n = grid.length;
+        int m = grid[0].length;
         
-        for(int i[][]:dp){
-            for(int j[]:i){
-                Arrays.fill(j,-1);
+        for(int j1 = 0; j1<m;j1++){
+            for(int j2 = 0; j2<m;j2++){
+                
+                if(j1==j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1]+grid[n-1][j2];
             }
         }
-            
         
-        return memo(0,0,grid[0].length-1,grid,dp);
+        
+        for(int i = grid.length-2;i>=0;i--){
+            for(int j1 = 0;j1<m;j1++){
+                for(int j2 = 0;j2<m;j2++){
+                    
+                    int maxi = Integer.MIN_VALUE;
+
+                    //Inner nested loops to try out 9 options
+                    for (int di = -1; di <= 1; di++) {
+                      for (int dj = -1; dj <= 1; dj++) {
+
+                        int ans;
+
+                        if (j1 == j2)
+                          ans = grid[i][j1];
+                        else
+                          ans = grid[i][j1] + grid[i][j2];
+
+                        if ((j1 + di < 0 || j1 + di >= m) ||
+                          (j2 + dj < 0 || j2 + dj >= m))
+
+                          ans += -1e9;
+                        else
+                          ans += dp[i + 1][j1 + di][j2 + dj];
+
+                        maxi = Math.max(ans, maxi);
+                      }
+                    }
+                    
+                    dp[i][j1][j2] = maxi;
+                }                
+            }
+        }
+                
+        return dp[0][0][grid[0].length-1];
     }
 }
