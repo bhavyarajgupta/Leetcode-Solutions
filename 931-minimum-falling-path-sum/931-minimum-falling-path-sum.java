81cf82377a5c296a3b06b2ce7e753605230f9693
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-09-30 16:07:17 +0530

    Time: 11 ms (22.60%), Space: 47.8 MB (36.86%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,32 +1,30 @@
 class Solution {
-    
-    public int memo(int i,int j,int[][] m,int [][]dp){
         
-        if(j<0 || j >= m[0].length) return (int)Math.pow(10,9);
-        if(i == 0) return m[0][j];
-        
-        if(dp[i][j] != 211) return dp[i][j];
-        
-        int s = m[i][j] + memo(i-1,j,m,dp);
-        int sl = m[i][j] + memo(i-1,j-1,m,dp);
-        int sr = m[i][j] + memo(i-1,j+1,m,dp);
-        
-        return dp[i][j] = Math.min(s,Math.min(sl,sr));
-    }
-    
     public int minFallingPathSum(int[][] matrix) {
        
-        
         int dp[][] = new int [matrix.length][matrix[0].length];
         
-        for(int k[]:dp){
-            Arrays.fill(k,211);
+        
+        for(int j = 0;j<matrix[0].length;j++) dp[0][j] = matrix[0][j];
+        
+        
+        for(int i=1;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                int s = matrix[i][j] + dp[i-1][j];
+                int sl = matrix[i][j] ;
+                    if(j-1>=0) sl+=dp[i-1][j-1];
+                    else sl += (int)Math.pow(10,9);;
+                int sr = matrix[i][j] ;
+                    if(j+1<matrix[0].length) sr+= dp[i-1][j+1];
+                    else sr += (int)Math.pow(10,9);;
+                 dp[i][j] = Math.min(s,Math.min(sl,sr));
+            }
         }
         
         int min = Integer.MAX_VALUE;
-        
         for(int j=0;j<matrix[0].length;j++)
-            min = Math.min(memo(matrix.length-1,j,matrix,dp),min);
+            min = Math.min(dp[matrix.length-1][j],min);
+        
         
         
         return min;
