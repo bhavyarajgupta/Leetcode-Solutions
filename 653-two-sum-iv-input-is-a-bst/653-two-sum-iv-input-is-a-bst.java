6a256e21be0f1aa8ff9b0b7cdd0a88ca3013041c
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-10-09 10:34:15 +0530

    Time: 8 ms (37.86%), Space: 52.3 MB (21.95%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -15,75 +15,17 @@
  */
 class Solution {
     
-    ArrayList<Integer> store ;
-    public void inOrder(TreeNode root){
-        if(root == null) return ;
-        
-        inOrder(root.left);
-        store.add(root.val);
-        inOrder(root.right);
-        
-    }
+    HashSet<Integer> set = new HashSet<>();
     
-//     public boolean findTarget(TreeNode root, int k) {
-//         store = new ArrayList<>();
+    public boolean findTarget(TreeNode root, int k) {
         
-//         inOrder(root);
-        
-//         int left = 0;
-//         int right = store.size()-1;
-        
-//         System.out.println(store);
-        
-//         while(left<right){
-//             int sum = store.get(left)+store.get(right);
-//             if(sum == k){
-//                 left++;
-//                 right--;
-//                 return true;                
-//             }else if(sum < k){
-//                 left++;
-//             }else{
-//                 right--;
-//             }
-            
-//         }
-        
-//         return false;
-//     }
-    boolean ans = false;
-    public boolean find(TreeNode root,int value){
         if(root == null) return false;
         
-        if(root.val == value) return true;
+        if(set.contains(k-root.val)) return true;
+        set.add(root.val);
         
-        if(root.val < value) return find(root.right,value);
-        return find(root.left,value);
+        return  findTarget(root.left,k) ||  findTarget(root.right,k);
+         
         
-        // return false;
-    }
-    
-    public boolean helper(TreeNode curr,int k,TreeNode root){
-        if(curr == null) return false;
-        
-        boolean leftRes = helper(curr.left,k,root);
-        
-        if(curr.val > k/2){
-            int complement = k - curr.val; 
-            if(find(root,complement) == true){
-                return true;
-            }
-        }
-        
-        
-        
-        boolean rightRes = helper(curr.right,k,root);
-        
-        return leftRes||rightRes;
-    }
-    public boolean findTarget(TreeNode root, int k) {
-        // ans = false;
-        return helper(root,k,root);
-        // return ans;
     }
 }
