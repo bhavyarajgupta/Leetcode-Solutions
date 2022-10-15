6e2e7e4a0a328fa853b1a35f9d78eb5e09fb83a0
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-10-16 00:58:08 +0530

    Time: 7 ms (63.39%), Space: 49.3 MB (44.31%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     
-    private int memorec(int idx ,int T,int[] coins,int dp[][]){
-        if(idx == 0){
-            return T%coins[0] == 0 ? 1:0;
-        }
-        
-        if(dp[idx][T] != -1) return dp[idx][T];
-        
-        int ntake = memorec(idx-1,T,coins,dp);
-        int take = 0;
-        if(coins[idx] <= T)
-            take = memorec(idx,T-coins[idx],coins,dp);
-        
-        return dp[idx][T] = take+ntake;
-    }
     
     public int change(int amount, int[] coins) {
         int dp [][] = new int [coins.length][amount+1];
+
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)
+            dp[0][i]=1;
+        }
         
-        for(int i[]:dp)
-            Arrays.fill(i,-1);
         
-       return memorec(coins.length-1,amount,coins,dp);
+        for(int i=1;i<coins.length;i++){
+            for(int j=0;j<=amount;j++){
+                int ntake = dp[i-1][j];
+                int take = 0;
+                if(coins[i] <= j)
+                    take = dp[i][j-coins[i]];
+
+                dp[i][j] = take+ntake;
+                
+            }
+        }
+      
+        return dp[coins.length-1][amount];
     }
 }
