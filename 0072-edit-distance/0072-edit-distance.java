766ebb2f59c5e81f87b3d21171bfd9180516673c
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-10-21 00:10:32 +0530

    Time: 13 ms (29.31%), Space: 45.3 MB (15.87%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -9,7 +9,6 @@ class Solution {
     
         if(dp[i][j] != -1) return dp[i][j];
         
-        
         if(w1.charAt(i-1) == w2.charAt(j-1)){
             return dp[i][j] = memo(i-1,j-1,w1,w2,dp);
         }
@@ -30,10 +29,30 @@ class Solution {
         
         int dp[][] = new int[n+1][m+1];
         
-        for(int i[]:dp) Arrays.fill(i,-1);
+//         for(int i[]:dp) Arrays.fill(i,-1);
         
-        return memo(n,m,word1,word2,dp);
+//         return memo(n,m,word1,word2,dp);
+        
+        for(int i=0; i<=n;i++) dp[i][0] = i;
+        for(int j=0; j<=m;j++) dp[0][j] = j;
         
         
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                 if(word1.charAt(i-1) == word2.charAt(j-1)){
+                   dp[i][j] = dp[i-1][j-1];
+                 }
+                 else{
+
+                    int insert = dp[i][j-1];
+                    int delete = dp[i-1][j];
+                    int replace = dp[i-1][j-1];
+
+                   dp[i][j] = 1+ Math.min(insert,Math.min(delete,replace));
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 }
