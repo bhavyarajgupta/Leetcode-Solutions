944a71a23d83b2a56c3becfac7137a8ec8a43fb6
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-10-21 00:25:43 +0530

    Time: 11 ms (47.32%), Space: 44 MB (83.23%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -23,36 +23,28 @@ class Solution {
         
     }
     
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        
-        int dp[][] = new int[n+1][m+1];
-        
-//         for(int i[]:dp) Arrays.fill(i,-1);
-        
-//         return memo(n,m,word1,word2,dp);
-        
-        for(int i=0; i<=n;i++) dp[i][0] = i;
-        for(int j=0; j<=m;j++) dp[0][j] = j;
-        
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                 if(word1.charAt(i-1) == word2.charAt(j-1)){
-                   dp[i][j] = dp[i-1][j-1];
-                 }
-                 else{
+    public int minDistance(String S1, String S2) {
+        int n = S1.length();
+        int m = S2.length();
 
-                    int insert = dp[i][j-1];
-                    int delete = dp[i-1][j];
-                    int replace = dp[i-1][j-1];
+        int[] prev=new int[m+1];
+        int[] cur=new int[m+1];
 
-                   dp[i][j] = 1+ Math.min(insert,Math.min(delete,replace));
-                }
-            }
+        for(int j=0;j<=m;j++){
+            prev[j] = j;
         }
-        
-        return dp[n][m];
+
+        for(int i=1;i<n+1;i++){
+            cur[0]=i;
+            for(int j=1;j<m+1;j++){
+                if(S1.charAt(i-1)==S2.charAt(j-1))
+                    cur[j] = 0+prev[j-1];
+
+                else cur[j] = 1+Math.min(prev[j-1],Math.min(prev[j],cur[j-1]));
+            }
+            prev = (int[])(cur.clone());
+        }
+
+        return prev[m];
     }
 }
