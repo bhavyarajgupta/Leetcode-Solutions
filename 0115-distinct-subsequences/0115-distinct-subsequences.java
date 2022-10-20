aefaf0300718e4f29560f171a6dce1e3a4d3436e
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-10-20 17:44:25 +0530

    Time: 45 ms (18.49%), Space: 49.9 MB (67.80%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -24,11 +24,29 @@ class Solution {
         
         int dp[][] = new int [n+1][m+1];
         
-        for(int i[]:dp){
-            Arrays.fill(i,-1);
+        for(int k[]:dp) Arrays.fill(k,0);
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        // for(int j=0;j<=m;j++){
+        //     dp[0][j] = 0;
+        // }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int ds = 0;
+                
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    ds = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    ds = dp[i-1][j];
+                }
+                
+                dp[i][j] = ds;
+            }
         }
         
-        return memo(n,m,s,t,dp);
+        return dp[n][m];
         
     }
 }
