83ea0746466b64143438634c6c0efd6bcac42c35
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-10-20 17:05:38 +0530

    Time: 19 ms (86.94%), Space: 49.6 MB (73.43%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    private int memo(int i,int j,String s, String t,int dp[][]){
+        
+        if(j<0) return 1;
+        if(i<0) return 0;      // because i is already exhausted an there may be a possiblity from j
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int ds = 0;
+        if(s.charAt(i) == t.charAt(j)){
+           ds = memo(i-1,j-1,s,t,dp) + memo(i-1,j,s,t,dp);
+        }else{
+            ds = memo(i-1,j,s,t,dp);
+        }
+        
+        return dp[i][j] = ds;
+        
+    }
+    
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        
+        int dp[][] = new int [n][m];
+        
+        for(int i[]:dp){
+            Arrays.fill(i,-1);
+        }
+        
+        return memo(n-1,m-1,s,t,dp);
+        
+    }
+}
