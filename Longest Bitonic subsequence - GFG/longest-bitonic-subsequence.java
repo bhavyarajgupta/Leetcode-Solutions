40ff033afc20549a139d732a38d6025494a893f3
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-11-05 23:58:00 +0530

    Added solution - LeetHub

---
+++ Longest Bitonic subsequence - GFG/longest-bitonic-subsequence.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine().trim();
+            String[] s1 = s.split(" ");
+            int[] nums = new int[n];
+            for(int i = 0; i < s1.length; i++)
+                nums[i] = Integer.parseInt(s1[i]);
+            Solution ob = new Solution();
+            int ans = ob.LongestBitonicSequence(nums);
+            System.out.println(ans);           
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int LongestBitonicSequence(int[] nums)
+    {
+        int dp1[] = new int[nums.length];
+        Arrays.fill(dp1,1);
+        
+      
+        for(int i =0 ;i<nums.length;i++){
+            for(int prev = 0; prev < i;prev++){
+                if( nums[prev] < nums[i]  && 1+dp1[prev] > dp1[i]  ){
+                    dp1[i] = 1+dp1[prev];
+                }
+            }
+            
+        }
+        
+        int dp2[] = new int[nums.length];
+        Arrays.fill(dp2,1);
+        
+        for(int i =nums.length-1; i>=0;i--){
+            for(int prev = nums.length-1; prev > i;prev--){
+                if( nums[prev] < nums[i]  && 1+dp2[prev] > dp2[i]  ){
+                    dp2[i] = 1+dp2[prev];
+                }
+            }
+            
+        }
+        
+        int maxi = 0;
+        for(int i=0;i<nums.length;i++){
+            maxi = Math.max(maxi,dp1[i]+dp2[i]-1);
+        }
+        
+        return maxi;
+    }
+}
