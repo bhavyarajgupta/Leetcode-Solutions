d75d091e79518edd993c7e8f72e63ecddabc83d9
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-11-20 21:30:28 +0530

    Time: 24 ms (49.27%), Space: 43.8 MB (79.79%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,103 +1,34 @@
 class Solution {
-    private String refine(String exp) {
-        StringBuilder refined = new StringBuilder("");
-        for(int i = 0; i < exp.length(); i++) {
-            char c = exp.charAt(i);
-
-            if(c == ' ') continue;
-            if(c == '+') {
-                if(refined.length() == 0 || refined.charAt(refined.length()-1) == '(' )
-                    refined.append( "0+");
-                else if(refined.charAt(refined.length()-1) == '+' || refined.charAt(refined.length()-1) == '-')
-                    continue;
-                else
-                    refined .append( c);
-            } else if(c == '-') {
-                if(refined.length() == 0 || refined.charAt(refined.length()-1) == '(' )
-                    refined.append( "0-");
-                else if(refined.charAt(refined.length()-1) == '+')
-                    refined.setCharAt(refined.length()-1,'-') ;
-                else if(refined.charAt(refined.length()-1) == '-')
-                    refined.setCharAt(refined.length()-1,'+') ;
-                else
-                    refined.append(c);
-            } else
-                    refined.append(c);
-
-        }
-
-        return refined.toString();
-    }
-        
     public int calculate(String s) {
-        s = refine(s);
-        Stack<Integer> operand = new Stack<>();
-        Stack<Character> operator = new Stack<>();
+    int result = 0;
+    
+    int sign = 1;
+    Stack<Integer> stack = new Stack<Integer>();
+    
+    for(int i=0;i<s.length();i++){
+        if (Character.isDigit(s.charAt(i))) {
+        int sum = s.charAt(i) - '0';
         
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-                        
-            if(ch == ' ') continue;
-            else if(ch >= '0' && ch <='9' ){
-                int num = 0;
-                while(i < s.length() && ch >= '0' && ch <='9' ){
-                    num = num*10+(ch - '0');
-                    i++;
-                    if(i < s.length())
-                        ch = s.charAt(i);
-                }
-                
-                i--;
-                operand.push(num);
+        while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                sum = sum * 10 + s.charAt(i + 1) - '0';
+                i++;
             }
-            else if(ch == '('){
-                operator.push(ch);
-            }else if(ch == ')'){
-                while(operator.peek() != '('){
-                    int a = operand.pop();
-                    int b = operand.pop();
-                    char op = operator.pop();
-                    
-                    if(op == '+'){
-                        operand.push(a+b);
-                    }else{
-                        operand.push(b-a);
-                    }
-                }
-                
-                operator.pop();
-            }else{
-                while(operator.size()>0 && operator.peek()!= '('){
-                    int a = operand.pop();
-                    int b = operand.pop();
-                    char op = operator.pop();
-                    
-                    if(op == '+'){
-                        operand.push(a+b);
-                    }else{
-                        operand.push(b-a);
-                    }
-                    
-                } 
-                
-                operator.push(ch);
-            }
-                        
+        result += sum * sign;
+             
+        }else if (s.charAt(i) == '+')
+            sign = 1;
+        else if (s.charAt(i) == '-')
+            sign = -1;
+        else if (s.charAt(i) == '(') {
+            stack.push(result);
+            stack.push(sign);
+            result = 0;
+            sign = 1;
+        } else if (s.charAt(i) == ')') {
+            result = result * stack.pop() + stack.pop();
         }
-         
-        while(operator.size() > 0){
-            int a = operand.pop();
-            int b = operand.pop();
-            char op = operator.pop();
 
-            if(op == '+'){
-                operand.push(a+b);
-            }else{
-                operand.push(b-a);
-            }
-        }
-        
-        return operand.pop();
-        
     }
+    return result;
+}
 }
