a6d6674e6b00557107f00a11584a10a63763fd7a
Bhavya Raj Gupta <90672316+bhavyarajgupta@users.noreply.github.com>
2022-12-19 17:42:40 +0530

    Time: 160 ms (62.85%), Space: 134.4 MB (89.27%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,68 +1,34 @@
 class Solution {
-    class Graph{
-        public ArrayList<Integer>[] adj;
-        
-        Graph(int n){
-            adj = new ArrayList[n];
-            
-            for(int i=0;i<n;i++){
-                adj[i] = new ArrayList<>();
-            }
-        }
-        
-        public void addEdge (int src,int des){
-            adj[src].add(des);
-            adj[des].add(src);
-        }
-    }
     
-    public  boolean DFS(int src,int desc,Graph g,boolean[] vis){
-        if(src == desc) return true;
-        if(vis[src] == true) return false;
-        vis[src] = true;
+    private boolean dfs(int source , int dest ,ArrayList<Integer>[] adjList ,boolean []vis){
+        if(vis[source]) return false;
+        if(source  == dest) return true;
         
-        for(int nbr:g.adj[src]){
-            if(DFS(nbr,desc,g,vis) == true){
+        vis[source] = true;
+        
+        for(Integer nbr:adjList[source]){
+            if(dfs(nbr,dest,adjList,vis)){
                 return true;
             }
-        }
-        
-        return false;
-    }
-    
-    
-    public boolean BFS(int src , int dest,Graph g){
-        Queue<Integer> q = new ArrayDeque<>();
-        boolean[] vis = new boolean[g.adj.length];
-        q.add(src);
-        
-        while(q.size() > 0){
-            int front = q.remove();
-            if(vis[front] == true) continue;
-            if(front == dest) return true;
-            vis[front] = true;
             
-            for(Integer nbr: g.adj[front]){
-                if(vis[nbr] == false){
-                    q.add(nbr);   
-                }
-            }
         }
-        
         return false;
     }
-    
+        
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Graph g = new Graph(n);
+        ArrayList<Integer>[]  adjList = new ArrayList[n];
+        
+        for(int i = 0;i<n;i++){
+            adjList[i] = new ArrayList<>();
+        }
         
         for(int i=0;i<edges.length;i++){
-            g.addEdge(edges[i][0],edges[i][1]);
+            adjList[edges[i][0]].add(edges[i][1]);
+            adjList[edges[i][1]].add(edges[i][0]);
         }
         
-        boolean []vis = new boolean[n];
+        boolean vis[] = new boolean[n];
         
-        // return DFS(source,destination,g,vis);
-        return BFS(source , destination,g);
-        
+        return dfs(source , destination , adjList , vis);
     }
 }
